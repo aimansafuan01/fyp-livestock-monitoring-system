029238aaedee3fb058c6382e3ae756ae64aa0ebf
aimansafuan01 <86760998+aimansafuan01@users.noreply.github.com>
2024-02-14 12:05:28 +0800

    Change port to 8080

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get('/daily-record', (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
