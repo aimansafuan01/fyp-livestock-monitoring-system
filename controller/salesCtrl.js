941a62b82018c074f366bc86c1946462c46c23db
aimansafuan01 <86760998+aimansafuan01@users.noreply.github.com>
2024-03-03 15:32:16 +0800

    Sales Commit 2 - Created create sales form page - Update breadcrumbs - Added Sales Record on left nav bar for every page

--- controller/salesCtrl.js
+++ controller/salesCtrl.js
@@ -1,6 +1,11 @@
 import * as SalesDB from '../db/record-surveillance.js';
 
-// Get surveillance Record Page
+// Get Sales Record Page
 export const getSalesRecordPage = async (req, res) => {
   res.status(200).render('sales');
+};
+
+// Get Sales Form Page
+export const getSalesForm = async (req, res) => {
+  res.status(200).render('create-sales-record');
 };
--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -3,7 +3,10 @@ import * as SalesCtrl from '../controller/salesCtrl.js';
 
 const router = express.Router();
 
-// View Sales
+// View Sales Record
 router.get('/view', SalesCtrl.getSalesRecordPage);
+
+// View Sales Form
+router.get('/create', SalesCtrl.getSalesForm);
 
 export default router;
